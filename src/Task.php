ee32bf37c825950dd7bd3cec9eacc0851738f85a
Brianna Popson <bpopson@gmail.com>
2016-02-22 11:02:41 -0800

    create passing specs and html for Category class

--- src/Task.php
+++ src/Task.php
@@ -50,5 +50,18 @@ class Task
         $GLOBALS['DB']->exec("DELETE FROM tasks;");
     }
 
+    static function find($search_id)
+    {
+        $found_task = null;
+        $tasks = Task::getAll();
+        foreach($tasks as $task) {
+          $task_id = $task->getId();
+          if ($task_id == $search_id) {
+              $found_task = $task;
+          }
+        }
+        return $found_task;
+    }
+
 }
 ?>
