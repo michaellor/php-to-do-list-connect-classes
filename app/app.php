9bd90c10d90b6e1ed0b7d2be4ccc290ec566c562
Brianna Popson and Michael Lor <bpopson@gmail.com>
2016-02-23 11:09:07 -0800

    add due_date functionality to entire app

--- app/app.php
+++ app/app.php
@@ -32,7 +32,8 @@ $app['debug'] = true;
     $app->post("/tasks", function() use ($app) {
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
-        $task = new Task($description, $id = null, $category_id);
+        $due_date = $_POST['due_date'];
+        $task = new Task($description, $id = null, $category_id, $due_date);
         $task->save();
         $category = Category::find($category_id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
@@ -40,7 +41,7 @@ $app['debug'] = true;
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('delete_tasks.html.twig');
     });
 
     $app->post("/categories", function() use ($app) {
@@ -51,7 +52,7 @@ $app['debug'] = true;
 
     $app->post("/delete_categories", function() use ($app) {
         Category::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('delete_categories.html.twig');
       });
 
     return $app;
